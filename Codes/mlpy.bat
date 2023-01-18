81a391ed816bc0c3ff640a7097023add58fbb636
a-mhamdi <a_mhamdi@outlook.com>
2023-01-19 00:30:14 +0100

    update files

--- Codes/mlpy.bat
+++ Codes/mlpy.bat
@@ -1,10 +1,10 @@
 start "" c:\mlpy\"Lab-Machine Learning (L3)".pdf
 ::LAUNCH CHROME
-start chrome /incognito 192.168.99.100:4321
+start chrome /incognito 192.168.99.100:1357
 ::START DOCKER DEAMON
 docker-machine start
 ::RUN A CONTAINER "l3" FROM IMAGE "mlpy"
-docker run --rm --name l3 -p 4321:4321 mlpy
+docker run --rm --name l3 -p 1357:1357 mlpy
 ::EXIT UPON COMPLETION
 exit
 
