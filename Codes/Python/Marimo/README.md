de4d8ab1a8a4b2ec0b7e56e640e0982753f370aa
Abdelbacet Mhamdi <a_mhamdi@outlook.com>
2024-12-20 22:37:40 +0100

    some changes

--- Codes/Python/Marimo/README.md
+++
@@ -1,3 +0,0 @@
-
-![Marimo](marimo.svg)
-
--- Codes/Python/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for filename in $(ls -la | grep .ipynb | awk '{print $9}');
-do
-        marimo convert $filename > ${filename:0:-6}.py
-done
